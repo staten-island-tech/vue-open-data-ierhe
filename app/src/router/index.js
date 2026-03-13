c16616dc17a60e13efcd3768c21f3e454a6f1fb3
ierhe <yuningw05@gmail.com>
2026-03-13 12:30:20 -0400

    First

---
+++ app/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router' 
+import MainFile from '@/views/MainFile.vue'
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path:'/',
+      name:'Main',
+      component: MainFile,
+    },
+  ],
+})
+
+export default router
---
+++ app/src/views/MainFile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const TData = ref([])
+async function getData(){
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    const data = await response.json()
+    stuff.value = data.results
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
