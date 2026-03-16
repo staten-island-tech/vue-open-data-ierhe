3bcc1c40a9347d6b4196281f99215c20b958735d
ierhe <yuningw05@gmail.com>
2026-03-16 12:07:05 -0400

    Create DataCard.vue

---
+++ app/src/components/DataCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <router-link :to="DataPath" class="card">
+        <h2>{{ Restruants.name }}</h2>
+        <h3>{{ id }}</h3>
+    </router-link>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    Restruants:{
+        type: Object, 
+        required: true 
+    }, 
+    id: {
+        type:Number,
+        required: true
+    }
+})
+const DataPath = computed(() =>{
+    return '/Main/${props.Restruants.name}'
+})
+</script>
+
+<style scoped>
+.card {
+  width: 28%;
+  height: 500px;
+  background-color: aliceblue;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  text-transform: uppercase;
+}
+img {
+  width: 100%;
+  height: 80%;
+  object-fit: cover;
+}
+</style>
