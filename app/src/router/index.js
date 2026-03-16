6fa29999a86c8644d03cb550578d090a5eb9f412
ierhe <yuningw05@gmail.com>
2026-03-16 12:07:03 -0400

    Update index.js

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/',
+      path:'/Main/:id',
       name:'Main',
       component: MainFile,
     },
