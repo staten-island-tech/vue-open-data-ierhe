c5e3b9069497fb309010c234a8173a159c2231ec
ierhe <yuningw05@gmail.com>
2026-03-16 12:07:00 -0400

    Update MainFile.vue

--- app/src/views/MainFile.vue
+++ app/src/views/MainFile.vue
@@ -9,13 +9,17 @@ import { ref, onMounted } from 'vue';
 const TData = ref([])
 async function getData(){
   try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    const response = await fetch('https://data.cityofnewyork.us/resource/9b9u-8989.json')
     const data = await response.json()
-    stuff.value = data.results
+    TData.value = data.results
   } catch (error) {
     console.log(error)
   }
 }
+onMounted(()=> {
+  getData()
+})
+
 </script>
 
 <style scoped>
