875ce99e2262a32e89dc6607de0967b98ec0e946
ierhe <yuningw05@gmail.com>
2026-03-17 12:16:44 -0400

    Update MainFile.vue

--- app/src/views/MainFile.vue
+++ app/src/views/MainFile.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-      <DataCard v-for="(Dat, index) in TData" :key="Dat.establishment_record_dba"
+    <div class="container">
+      <DataCard v-for="(Dat, index) in TData" :key="Dat.business_name"
       :TData="Dat" :id="index + 1"/>
     </div>
 </template>
@@ -11,9 +11,11 @@ import DataCard from '@/components/DataCard.vue';
 const TData = ref([])
 async function getData(){
   try {
-    const response = await fetch('https://data.cityofnewyork.us/resource/9b9u-8989.json')
+    const response = await fetch('https://data.cityofnewyork.us/resource/5vi6-xdpy.json')
     const data = await response.json()
     TData.value = data.results
+    console.log("works")
+
   } catch (error) {
     console.log(error)
   }
@@ -26,5 +28,13 @@ onMounted(()=> {
 </script>
 
 <style scoped>
-
+.container {
+  width: 80vw;
+  margin: 30px auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
 </style>
