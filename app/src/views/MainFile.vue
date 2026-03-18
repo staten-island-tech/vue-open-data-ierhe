80863ecd1f0330b49e5543f54bd116181b7b9450
ierhe <yuningw05@gmail.com>
2026-03-18 12:06:40 -0400

    Update MainFile.vue

--- app/src/views/MainFile.vue
+++ app/src/views/MainFile.vue
@@ -14,8 +14,7 @@ async function getData(){
     const response = await fetch('https://data.cityofnewyork.us/resource/5vi6-xdpy.json')
     const data = await response.json()
     TData.value = data
-    console.log("works")
-
+    
   } catch (error) {
     console.log(error)
   }
