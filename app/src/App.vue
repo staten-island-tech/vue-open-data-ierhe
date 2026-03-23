62e009554abeb2ccef4bfe0db27d33c7137cd11f
ierhe <yuningw05@gmail.com>
2026-03-23 12:21:23 -0400

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <RouterLink to="/ChartsPage">View the charts</RouterLink>
+  <RouterLink to="/charts">View the charts</RouterLink>
+  <RouterLink to="/">View of all tabs</RouterLink>
   <RouterView />
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router';
+
+
 
 </script>
 
