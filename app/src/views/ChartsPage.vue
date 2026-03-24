a3026f1691f5f841137cad6c29b037727a32090a
ierhe <yuningw05@gmail.com>
2026-03-24 12:27:12 -0400

    Update ChartsPage.vue

--- app/src/views/ChartsPage.vue
+++ app/src/views/ChartsPage.vue
@@ -5,7 +5,22 @@
 <script setup>
 
 import BarChart from '@/components/BarChart.vue';
+import { ref, onMounted } from 'vue';
 
+const TData = ref([])
+async function getData(){
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/5vi6-xdpy.json')
+    const data = await response.json()
+    TData.value = data
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+onMounted(()=> {
+  getData()
+})
 
 </script>
 
