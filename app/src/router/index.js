2439135d54ca82861193048d9cf280d8bd59c904
ierhe <yuningw05@gmail.com>
2026-03-27 12:21:19 -0400

    Update index.js

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router' 
 import MainFile from '@/views/MainFile.vue'
 import ChartsPage from '@/views/ChartsPage.vue'
+import OtherPage from '@/components/OtherChart.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +14,11 @@ const router = createRouter({
       path: '/charts',
       name: 'Charts',
       component: ChartsPage,
+    },
+    {
+      path: '/Other',
+      name: 'other',
+      component: OtherPage,
     }
   ],
 })
