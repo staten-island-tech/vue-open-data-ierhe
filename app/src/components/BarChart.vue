0e3aefaacf7488966677f7ec73f173542ef8e299
ierhe <yuningw05@gmail.com>
2026-03-27 12:21:13 -0400

    Update BarChart.vue

--- app/src/components/BarChart.vue
+++ app/src/components/BarChart.vue
@@ -6,7 +6,7 @@
   />
 </template>
 
-<script >
+<script>
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
