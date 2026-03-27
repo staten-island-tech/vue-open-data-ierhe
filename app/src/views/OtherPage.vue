6d829923de8f2af3c0353c03608d004cf2156861
ierhe <yuningw05@gmail.com>
2026-03-27 12:21:21 -0400

    Create OtherPage.vue

---
+++ app/src/views/OtherPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <OtherChart />
+    </div>
+</template>
+
+<script setup>
+import OtherChart from '@/components/OtherChart.vue';
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
